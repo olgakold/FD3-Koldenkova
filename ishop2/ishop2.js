13664355beecc9eae11680122bdc5101c1ef329f
olgakold <64192555+olgakold@users.noreply.github.com>
2020-05-02 11:58:45 +0300

    Update and rename ishop2/ishop2.js to  ishop2/ishop2.js

--- ishop2/ishop2.js
+++ ishop2/ishop2.js
@@ -3,11 +3,13 @@ var IShop = React.createClass({
     displayName: 'IShop',
   
     render: function() {  
-                 
-      
+
+      var icesCode=this.props.products.map( i =>
+      React.createElement(Products, {key:i.code, nameice:i.nameice, price:i.price, count:i.count, url:i.url, foto:i.foto} )
+    ); 
       return React.DOM.div( {className:'IShop'}, 
       React.DOM.div( {className:'NameStore'}, this.props.name ),
-      React.DOM.createElement(Products),
+      React.DOM.table ({className:'df'},React.DOM.tbody({className:'xcx'}, icesCode)),
     );
   },
     
