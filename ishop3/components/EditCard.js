786e4a35de17f0e1cbbc6a1c30413e62c95f14ff
olgakold <64192555+olgakold@users.noreply.github.com>
2020-05-14 23:04:39 +0300

    Update EditCard.js

--- ishop3/components/EditCard.js
+++ ishop3/components/EditCard.js
@@ -10,34 +10,36 @@ class EditCard extends React.Component {
         icesCard: PropTypes.array.isRequired,   
         nameErrorText: PropTypes.string,
         priceErrorText: PropTypes.string,
-        isValid: PropTypes.bool
+        isValidName: PropTypes.bool,
+        isValidPrice: PropTypes.bool
     }
 
     state ={
         icesCard: this.props.icesCard,
         nameErrorText: '',
         priceErrorText: '',
-        isValid: true
+        isValidName: true,
+        isValidPrice: true
     }
 ChangeNameText = (EO) =>{
     var currentValue=EO.target.value
     if (currentValue==""){
-       this.setState({nameErrorText:'Error', isValid:false})
+       this.setState({nameErrorText:'Error', isValidName:false})
        
     }
     else {
-        this.setState({nameErrorText:'', isValid:true})
+        this.setState({nameErrorText:'', isValidName:true})
     }
     this.setState({value: currentValue})
 }
 ChangePriceText = (EO) =>{
     var currentValue=EO.target.value
     if (currentValue==""){
-       this.setState({priceErrorText:'Error', isValid:false})
+       this.setState({priceErrorText:'Error', isValidPrice:false})
        
     }
     else {
-        this.setState({priceErrorText:'', isValid:true})
+        this.setState({priceErrorText:'', isValidPrice:true})
     }
     this.setState({value: currentValue})
 }
@@ -56,12 +58,12 @@ render (){
       <div className='EditName'>
         <label>Name</label>
         <input defaultValue={this.props.icesCard[0].nameice} onChange={this.ChangeNameText}></input>
-        <span className='NameError'>{this.state.nameErrorText}</span>
+        <div className='NameError'>{this.state.nameErrorText}</div>
       </div>
       <div className='EditPrice'>
       <label>Price</label>
         <input defaultValue={this.props.icesCard[0].price} onChange={this.ChangePriceText}></input>
-        <span className='NameError'>{this.state.priceErrorText}</span>
+        <div className='NameError'>{this.state.priceErrorText}</div>
       </div>
       <div className='EditURL'>
         <label>URL</label>
@@ -71,7 +73,7 @@ render (){
       <label>Quantity</label>
         <input defaultValue={this.props.icesCard[0].count}></input>      
       </div>
-      <input className='EditCardSave' type='button' value='Save' disabled={!this.state.isValid}></input>
+      <input className='EditCardSave' type='button' value='Save' disabled={!this.state.isValidName||!this.state.isValidPrice}></input>
       <input className='EditCardCancel' type='button' value='Cancel'></input>
 
       </div>
