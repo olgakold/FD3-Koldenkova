cb7b36b9f35463f0f2973d62d2e9934af17c63e0
olgakold <64192555+olgakold@users.noreply.github.com>
2020-05-14 22:34:53 +0300

    Update IceCard.js

--- ishop3/components/IceCard.js
+++ ishop3/components/IceCard.js
@@ -7,26 +7,24 @@ import './IceCard.css';
 class IceCard extends React.Component {
 
     static propTypes = {
-        nameice: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired,
-        url: PropTypes.string.isRequired,
-        foto: PropTypes.string.isRequired,
-        count: PropTypes.number.isRequired,
-
-      }    
+        icesCard: PropTypes.array.isRequired,   
+     
+    }
 
 render (){
-    return (
-    <div className='IceCard'>
-      <h3 className='NameIceCard'>{this.props.nameice}</h3>
-      <div className='NameIcePrice'>Price: {this.props.price}</div>
-      <a className='NameIceURl' href={this.props.url} target='_blank'>{this.props.foto} </a>  
-      <div className='NameIceCount'>Quantity: {this.props.count}</div>
+      
+      return ( 
+      <div className='IceCard'>
+      <h3 className='NameIceCard'>{this.props.icesCard[0].nameice}</h3>
+      <div className='NameIcePrice'>Price: {this.props.icesCard[0].price}</div>
+      <a className='NameIceURl' href={this.props.icesCard[0].url} target='_blank'>{this.props.icesCard[0].foto} </a>  
+      <div className='NameIceCount'>Quantity: {this.props.icesCard[0].count}</div>
     
     </div>
     )
+    
+   
+  
 }
-
-
 }
 export default IceCard;
