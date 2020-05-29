5247123c9f05387cc7206d42f6950724a0ff8abd
olgakold <64192555+olgakold@users.noreply.github.com>
2020-05-29 16:41:28 +0300

    Update withRainbowFrame.js

--- RainbowFrameHOC/components/withRainbowFrame.js
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -8,13 +8,12 @@ function withRainbowFrame (colors){
 return function(Component) {
   return function (props) {
   var elem=<Component {...props}/>
-  var divArr=[] 
   colors.forEach ((i,v)=>{                 
       elem=<div style={{border:"solid 4px "+i, padding:"10px",  width:270+40*v, marginLeft:"6px"}}  key={v} children={elem}></div> 
          
   })
-  divArr.push(elem)
-  return  divArr
+ 
+  return  elem
   
   }
 };
